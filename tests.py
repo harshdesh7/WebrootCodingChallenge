b6c28e23edb5d1a2593a47d04991f85c0c3335ae
Harsh Deshpande <hade7393@colorado.edu>
2021-02-17 13:45:12 -0700

    Added all test cases editiing db file

--- tests.py
+++ tests.py
@@ -21,6 +21,55 @@ class TestServerFunctionality(unittest.TestCase):
 
         self.assertEqual(usr["tstUsr"], "password")
 
+    def test_addUserToFilledDB(self):
+
+        pick_off = open("db_sim.pickle", "rb")
+        usr = pickle.load(pick_off)
+        pick_off.close()
+
+        usr["tstUsr2"] = "tc2"
+
+        # put dictionary back into pickle file (equivalent to DB modification)
+        pick = open("db_sim.pickle", "wb")
+        pickle.dump(usr, pick)
+        pick.close()
+
+        # get dictionary back (equivalent to DB query)
+        pick_off = open("db_sim.pickle", "rb")
+        retrieved = pickle.load(pick_off)
+        pick_off.close()
+
+        self.assertEqual(retrieved["tstUsr"], "password")
+        self.assertEqual(retrieved["tstUsr2"], "tc2")
+
+    def test_deleteUserFromDB(self):
+
+        pick_off = open("db_sim.pickle", "rb")
+        usr = pickle.load(pick_off)
+        pick_off.close()
+
+        del usr["tstUsr2"] #remove entry from dictionary
+
+        # put dictionary back into pickle file (equivalent to DB modification)
+        pick = open("db_sim.pickle", "wb")
+        pickle.dump(usr, pick)
+        pick.close()
+
+        # get dictionary back (equivalent to DB query)
+        pick_off = open("db_sim.pickle", "rb")
+        retrieved = pickle.load(pick_off)
+        pick_off.close()
+
+        self.assertEqual(retrieved["tstUsr"], "password")
+        self.assertTrue("tstUsr2" not in retrieved)
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
 
