5b84796069d4736ea74587ce769ffb968060c12b
Harsh Deshpande <hade7393@colorado.edu>
2021-02-17 20:00:47 -0700

    removed all cookie sim tests, as cookie sim ended up not being needed

--- tests.py
+++ tests.py
@@ -73,62 +73,6 @@ class TestServerFunctionality(unittest.TestCase):
 
         self.clearFileHelper("db_sim.pickle") #clear db file
 
-    def test_addToEmptyCookieSet(self):
-
-        self.clearFileHelper("cookie_sim.pickle")
-        cookieJar: set[str] = set(["cookie1"]) #cookie jar will be set of cookies
-
-        pick = open("cookie_sim.pickle", "wb")
-        pickle.dump(cookieJar, pick)
-        pick.close()
-
-        pick_off = open("cookie_sim.pickle", "rb")
-        cJar = pickle.load(pick_off)
-        pick_off.close()
-
-        self.assertTrue("cookie1" in cJar)
-
-    def test_addToFilledCookieSet(self):
-
-        pick_off = open("cookie_sim.pickle", "rb")
-        cJar = pickle.load(pick_off)
-        pick_off.close()
-
-        cJar.add("cookie2")
-
-        pick = open("cookie_sim.pickle", "wb")
-        pickle.dump(cJar, pick)
-        pick.close()
-
-        pick_off = open("cookie_sim.pickle", "rb")
-        cJar = pickle.load(pick_off)
-        pick_off.close()
-
-        self.assertTrue("cookie1" in cJar)
-        self.assertTrue("cookie2" in cJar)
-
-    def test_deleteFromCookieSet(self):
-
-        pick_off = open("cookie_sim.pickle", "rb")
-        cJar = pickle.load(pick_off)
-        pick_off.close()
-
-        self.assertTrue("cookie2" in cJar)
-        cJar.remove("cookie2")
-
-        pick = open("cookie_sim.pickle", "wb")
-        pickle.dump(cJar, pick)
-        pick.close()
-
-        pick_off = open("cookie_sim.pickle", "rb")
-        cJar = pickle.load(pick_off)
-        pick_off.close()
-
-        self.assertTrue("cookie1" in cJar)
-        self.assertTrue("cookie2" not in cJar)
-
-        self.clearFileHelper("cookie_sim.pickle") #clear cookie file
-
     def test_canHitMainPage(self):
 
         response = self.mock.get("/")
@@ -136,11 +80,39 @@ class TestServerFunctionality(unittest.TestCase):
         #default page redirects to login, so code should be 302
         self.assertEqual(response.status_code, 302)
 
-    def testCanHitRegisterPage(self):
+    def test_canHitRegisterPage(self):
 
         response = self.mock.get("/register")
 
         self.assertEqual(response.status_code, 200)
+
+    def test_canRegisterUser(self):
+
+        response = self.mock.post('/register', data=dict(nm="mockUsr", pwd="password"), follow_redirects=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Successfully Registered User!', response.data)
+
+    def test_cannotReRegisterUser(self):
+
+        response = self.mock.post('/register', data=dict(nm="mockUsr", pwd="password2"), follow_redirects=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'User Already in DB', response.data)
+
+    def test_cannotLogInWithWrongPassword(self):
+
+        response = self.mock.post('/login', data=dict(nm="mockUsr", pwd="password2"), follow_redirects=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Wrong Password', response.data)
+
+    def test_cannotLogInUnregisteredUser(self):
+
+        response = self.mock.post('/login', data=dict(nm="mockUsr2", pwd="password2"), follow_redirects=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'User Not Found', response.data)
 
 if __name__ == '__main__':
     unittest.main()
